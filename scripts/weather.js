fe0fcdb48dbb6a89d7fc2b74f46a963fc9287fe0
alexandre1830 <alexandre.olv.sz@gmail.com>
2026-03-27 08:31:50 -0300

    Week 03

---
+++ scripts/weather.js
@@ -0,0 +1,26 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid={c2a96ee71799a313f9c4added01cce66}';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      displayResults(data); 
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    console.log('Hello')
+}
